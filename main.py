b27d91bb05ee9b3137665b95fe58a667d0f84402
Muse.Morgan <musemorgan@yahoo.com>
2025-11-12 16:29:21 -0500

    dev

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 #This will serve as the main coding elements for my personal task tracker project
 
-#Importing the API
+#Importing the FastAPI class from fastapi module, look into what module is?
 from fastapi import FastAPI # similar to SQL, but not sure of significance
 app = FastAPI() #defines my new app that im creating
 #app is an object, it is where interaction between the client and browser take place
+#fast api is a webframework, something that helps to simplify api creation [look to see if webframework is just a specialized api?]
 
 #defining a path
-@app.get("/")# lines 9-10 is an app decorator, defines http location so it will take us to whatever route place in here
+@app.get("/get-message")
 def root():
-        return {"Hello World"}
+        return {"Hello" : "World"}
 #def root & return a function ? look more into this
+# lines 10 is a route decorator, defines http location that will serve as the endpoint [in this case get-message]
+
+
