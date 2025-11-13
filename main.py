fabe882cc085a14001fdbcad37d99be8c8070744
Muse.Morgan <musemorgan@yahoo.com>
2025-11-12 22:58:13 -0500

    param

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 # This will serve as the main coding elements for my personal task tracker project
 
 # Importing the FastAPI class from fastapi module, look into what module is?
-from fastapi import FastAPI 
+#from fastapi import FastAPI 
 
-app = FastAPI() 
+#app = FastAPI() 
 # 5 defines my new app that im creating
 # app is an object, it is where interaction between the client and browser take place
 # fast api is a webframework, something that helps to simplify api creation [look to see if webframework is just a specialized api?]
 
 # defining a path
-@app.get("/")
-def read_root():
-        return {"Message": "Congrats! This is your first API"}
+#@app.get("/")
+#def read_root():
+#       return {"Message": "Congrats! This is your first API"}
 # def root & return a function ? look more into this
 # lines 10 is a route decorator, defines http location that will serve as the endpoint [in this case get-message]
 # what is placed above works in the terminal and postman, is the sole purpose of postman for checking?
+
+# passing a parameter
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/get")
+def hello(name: str):
+    return {'Message': "Congrats " + name + '! This is your first API!'}
