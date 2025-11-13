b85a08bfced12712a710cd6d922b70c0795f3c9e
Muse.Morgan <musemorgan@yahoo.com>
2025-11-12 22:37:05 -0500

    get route (error check)

--- main.py
+++ main.py
@@ -2,15 +2,16 @@
 
 # Importing the FastAPI class from fastapi module, look into what module is?
 from fastapi import FastAPI 
+
 app = FastAPI() 
 # 5 defines my new app that im creating
 # app is an object, it is where interaction between the client and browser take place
 # fast api is a webframework, something that helps to simplify api creation [look to see if webframework is just a specialized api?]
 
 # defining a path
-@app.get("/")
+@app.get("/get-message")
 def read_root():
         return {"Message": "Congrats! This is your first API"}
 # def root & return a function ? look more into this
 # lines 10 is a route decorator, defines http location that will serve as the endpoint [in this case get-message]
-
+# what is placed above works in the terminal and postman, is the sole purpose of postman for checking?
