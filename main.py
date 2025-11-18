9bd173a8a6f1c54de8e60ac92c1bcbc055bacdbf
Muse.Morgan <musemorgan@yahoo.com>
2025-11-18 13:28:43 -0500

    dv

--- main.py
+++ main.py
@@ -22,7 +22,9 @@
 
 # passing a parameter
 from fastapi import FastAPI
+
 app = FastAPI() 
+
 @app.get("/get-message") 
 def hello(name: str):
     return {'Message': "Congrats " + name + '! This is your first API!'}
